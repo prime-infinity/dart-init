c9e57575735b87a1c514357a6e64920e65cda1ce
prime-infinity <soe.a.prime@gmail.com>
2024-09-27 06:50:59 -0700

    done classes

--- class.dart
+++ class.dart
@@ -8,6 +8,8 @@ void main() {
 
   print(noodles.format());
   print(pizza.format());
+
+  var new_pizza = Pizza(["mushrooms", "pineapple"], "mushpizza", 15.99);
 }
 
 class MenuItem {
@@ -20,4 +22,11 @@ class MenuItem {
   String format() {
     return '$title --> $price'; //note that "this" is not used
   }
+}
+
+//a child class that extends the parent class
+class Pizza extends MenuItem {
+  List<String> toppings;
+
+  Pizza(this.toppings, super.title, super.price); //init the super vars
 }
